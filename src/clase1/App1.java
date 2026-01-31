aa2b74c8ee7e98324d24c07340027d7c147a4e83
SelvaBenitez <selva.benitezp05@gmail.com>
2026-01-31 14:18:09 -0500

    primer comentario

---
+++ src/clase1/App1.java
@@ -0,0 +1,14 @@
+package clase1;
+
+public class App1 {
+
+    /*public static void mensaje(String){
+        System.out.println("");
+    };*/
+
+    public static void main(String[] args){
+        System.out.println("hola");
+    };
+}
+
+
