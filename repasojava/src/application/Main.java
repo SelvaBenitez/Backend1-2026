4454e9521d607368f3fc9a58213378f1438c9948
SelvaBenitez <selva.benitezp05@gmail.com>
2026-02-07 12:04:21 -0500

    Creacion Main

---
+++ repasojava/src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void msg(String msg){
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+        msg("Ingrese el nombre del usuario");
+    }
+}
