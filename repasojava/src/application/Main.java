c464da7cc2224bd82bc095f5e50ea1959e2af488
SelvaBenitez <selva.benitezp05@gmail.com>
2026-02-14 12:04:58 -0500

    variables y metodos

--- repasojava/src/application/Main.java
+++ repasojava/src/application/Main.java
@@ -1,14 +1,36 @@
 package application;
-
 import java.util.Scanner;
-
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    public static void msg(String msg){
-        System.out.println(msg);
-    }
 
-    public static void main(String[] args) {
-        msg("Ingrese el nombre del usuario");
-    }
+    Boolean femenino;
+    int edad;
+    String nombre;
+
+    // calcular edad
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int anioActual;
+            int anioNacimiento;
+
+            System.out.print("Ingrese el año actual: ");
+            anioActual = scanner.nextInt();
+
+            System.out.print("Ingrese su año de nacimiento: ");
+            anioNacimiento = scanner.nextInt();
+
+            calcularEdad(anioActual, anioNacimiento);
+
+            scanner.close();
+        }
+
+        public static void calcularEdad(int anioActual, int anioNacimiento) {
+            int edad = anioActual - anioNacimiento;
+            System.out.println("EDAD: " + edad);
+        }
+
+        //contador numeros de 1 al 10
+
+
 }
--- src/clase1/App1.java
+++ src/clase1/App1.java
@@ -1,14 +1,29 @@
 package clase1;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*Scoop*/
 
 public class App1 {
 
-    /*public static void mensaje(String){
-        System.out.println("");
-    };*/
+    final static int ANNO_ACTUAL = 2026;
+/*No devuelve respuesta*/
+    public static void msg(String msg){
+        System.out.println(msg);
+    }
 
-    public static void main(String[] args){
-        System.out.println("hola");
-    };
+   public static void main(String[] args) {
+       final String MENSAJE_BIENVENIDO = "Bienvenidos a este nuevo semestre";
+       String[] nombreEstudiantes = {"Isabel", "Jesus", "Gabriela", "Esteban", "Pedro"};
+       int[] anosNacimiento = {2003, 2004, 2005, 2006, 2007};
+
+       list<int[]> edadEstudiantes = ArrayList<>();
+   }
+/*Retorna elemento*/
+   static int calcularEdad(int anosNacimiento){
+        return ANNO_ACTUAL - anosNacimiento;
+   }
 }
 
 
